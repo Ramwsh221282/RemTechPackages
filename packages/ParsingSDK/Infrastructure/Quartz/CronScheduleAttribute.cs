a49859d77e402d17a81220d12d38984be367e0a0
Ramwsh221282 <jimkrauz@gmail.com>
2025-12-06 20:34:31 +0700

    removed postgre sql, rabbitmq, quartz dependencies

--- packages/ParsingSDK/Infrastructure/Quartz/CronScheduleAttribute.cs
+++
@@ -1,7 +0,0 @@
-namespace RemTechAvitoVehiclesParser.SharedDependencies.Quartz;
-
-[AttributeUsage(AttributeTargets.Class)]
-public sealed class CronScheduleAttribute(string schedule) : Attribute
-{
-    public string Schedule { get; } = schedule;
-}
--- packages/ParsingSDK/Infrastructure/Quartz/ICronScheduleJob.cs
+++
@@ -1,5 +0,0 @@
-﻿using Quartz;
-
-namespace RemTechAvitoVehiclesParser.SharedDependencies.Quartz;
-
-public interface ICronScheduleJob : IJob;
